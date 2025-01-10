800f6798b5764e0e55537eab7c71d594eb955113
cybergomez <3838275+cybergomez@users.noreply.github.com>
2025-01-09 21:36:27 -0500

    incorporar Tailwind CSS

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
+
 const Home = () => {
     return(
-        <div>
-            <h1>Welcome to the <b>RentaCar</b></h1>
-            <p>This is the home page of our React app.</p>
+        <div className="bg-amber-400">
+            <h1 className="text-3xl text-red-500 font-bold">Welcome to the <b>RentaCar</b></h1>
+            <p className="italic hover:not-italic">This is the home page of our React app.</p>
         </div>
     
     );
